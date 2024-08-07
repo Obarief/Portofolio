0ee0335f15621e3704d23f597ce13ed40c28d8a9
Obarief <oktavariba@gmail.com>
2024-08-08 00:24:31 +0700

    menambahkan fitur untuk validasi nama, email, dan nomor telepon

--- contact/app.js
+++ contact/app.js
@@ -1,16 +1,76 @@
-const {pertanyaan1, pertanyaan2, pertanyaan3, simpanKontak} = require('./contacts') // destructuring, untuk menangkap data dari file yang diexportkan yaitu contacts
+// membuat algoritma yang lebih simple
 
-//membuat fungsi utama untuk menjalankan CLI
-const main = async () => {
-    const nama = await pertanyaan1();
-    const nim = await pertanyaan2();
-    const noHP = await pertanyaan3();
+// mengambil argumen dari CLI
+
+// console.log(process.argv)
+// process dimiliki oleh nodejs
+// argv adalah isi dari function process
+// didalamnya ada array pada index-0 = directory penyimpanan nodejs
+// index-1 directory file yang dijalankan
+// index-2 output yang ditampilkan pada perintah ke 3 (contoh : node[0] app[1] oktavari[2] budi[3]) maka yg diambil oktavari
+// index-3 output yang di tampilkan pada perintah ke 4 yaitu budi
+
+
+
+// yargs.command('add','Menambahkan kontak baru', () => {}, (argv) => {
+//     console.log(argv.nama)
+//     // pada console ketik seperti ini = 'node app add --nama='oktavari' maka yang menjadi output adalah oktavari
+//     // -- akan menjadikan suatu output menjadi object
+// });
+
+// yargs.parse(); // untuk menjalankan npm yargs
+// // yargs.command('cmd','deskripsi','function/object(builder)','handler')
+
+const yargs = require('yargs')
+const { simpanKontak } = require('./contacts')
+yargs.command({
+    command: 'add',
+    describe: '*Menambahkan kontak baru*',
+    builder: {
+        nama: {
+            describe: 'Masukkan nama',
+            demandOption: true,
+            type: 'string'
+        },
+        email: {
+            describe: 'Masukkan email',
+            demandOption: false,
+            type: 'string'
+        },
+        noHP: {
+            describe: 'Masukkan nomor HP',
+            demandOption: true,
+            type: 'string'
+        }
+
+    },
+    handler(argv){
+        simpanKontak(argv.nama, argv.email, argv.noHP)
+    }
+})
+
+.parse();
+
+
+
+
+
+
+// abaikan program dibawah ini
+
+// const {pertanyaan1, pertanyaan2, pertanyaan3, simpanKontak} = require('./contacts') // destructuring, untuk menangkap data dari file yang diexportkan yaitu contacts
+
+// //membuat fungsi utama untuk menjalankan CLI
+// const main = async () => {
+//     const nama = await pertanyaan1();
+//     const nim = await pertanyaan2();
+//     const noHP = await pertanyaan3();
     
-    simpanKontak(nama, nim, noHP);
-};
+//     simpanKontak(nama, nim, noHP);
+// };
 
-//panggil fungsi main
-main();
+// //panggil fungsi main
+// main();
 
 
 
