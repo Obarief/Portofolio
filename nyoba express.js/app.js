2dd53a02d99a137bcea1a30f1f955a08b2a650bb
Oktavari Budi Arief <oktavariba@gmail.com>
2024-10-01 10:56:29 +0700

    add .gitognore

---
+++ nyoba express.js/app.js
@@ -0,0 +1,12 @@
+// import express from 'express';
+// const app = express();
+// const port = 3000;
+
+// app.get('/Intro',(req, res) => {
+//     res.send('halo dunia');
+// });
+
+// app.listen(port, () => {
+//     console.log(`server sudah menyala dalam port: ${port}`)
+// })
+
